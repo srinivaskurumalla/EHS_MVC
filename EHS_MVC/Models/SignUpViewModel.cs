5fb62465753ed31ddeeb0985c25d67543f1fefbf
pawans <pawans@ICS-LAP-3101>
2023-01-29 12:54:04 +0530

    Adding Post property /editing /details

---
+++ EHS_MVC/Models/SignUpViewModel.cs
@@ -0,0 +1,40 @@
+﻿using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EHS_MVC.Models
+{
+    public class SignUpViewModel
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Mobile Number")]
+        public long MobileNo { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter your Password")]
+        [RegularExpression("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$", ErrorMessage = "Invalid Email Id")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "please confirm your password")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password doesn't match")]
+        public string  ConfirmPassword { get; set; }
+
+       
+
+       
+    }
+}
